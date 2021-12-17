594e3434e4f2f94fb3bfed265cab4dc015c46ea9
Yuvraj883 <kingyuvi88@gmail.com>
2021-12-17 16:50:56 +0530

    Added javascript file

---
+++ app.js
@@ -0,0 +1,20 @@
+//alert("Hello Yuvi");
+var inputText =  document.querySelector("#input");
+var translateBtn = document.querySelector("#btn-translate");
+var outputText= document.querySelector("#output");
+
+
+
+function clickHandler(){
+ var url = constructURL(inputText.value);
+ fetch (url)
+ .then(function responseHandler(response){return response.json()})
+ .then (function logJson(json){outputText.innerText=json.contents.translated})
+};
+function constructURL(txt){
+return apiURL+"?"+"text="+txt;
+}
+
+translateBtn.addEventListener("click", clickHandler);
+
+var apiURL = "https://funtranslations.com/api/minion";
